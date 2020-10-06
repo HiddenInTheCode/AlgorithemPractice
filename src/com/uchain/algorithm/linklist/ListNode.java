35d9721afa9da4b4493c42be83797b961c9f39e6
YuYan Peng <68393503+HiddenInTheCode@users.noreply.github.com>
2020-10-06 23:14:17 +0800

    new algorithm

--- src/com/uchain/algorithm/linklist/ListNode.java
+++ src/com/uchain/algorithm/linklist/ListNode.java
@@ -2,12 +2,12 @@ package com.uchain.algorithm.linklist;
 
 public class ListNode {
     public int value;
-    public int next;
+    public ListNode next;
     public ListNode(){
 
     }
     public ListNode(int value){this.value=value;}
-    public ListNode(int value,int next){
+    public ListNode(int value,ListNode next){
         this.value=value;
         this.next=next;
     }
