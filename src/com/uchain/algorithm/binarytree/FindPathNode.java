12675c72ca8431ef50316439884212b89e4a3dc3
YuYan Peng <68393503+HiddenInTheCode@users.noreply.github.com>
2020-11-03 11:21:13 +0800

    二叉树中和为某一值的路径

---
+++ src/com/uchain/algorithm/binarytree/FindPathNode.java
@@ -0,0 +1,14 @@
+package com.uchain.algorithm.binarytree;
+
+/**
+ * @description leetcode 二叉树中和为某一值的路径
+ */
+public class FindPathNode {
+    int val=0;
+    FindPathNode left=null;
+    FindPathNode right=null;
+
+    public FindPathNode(int val){
+        this.val=val;
+    }
+}
